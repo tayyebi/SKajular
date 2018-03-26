908b60bc0b7135baba9840c1bf2c215bc495765f
tayyebi <tayyebimohammadreza@gmail.com>
2018-03-27 01:28:05 +0430

    sessions

--- schedule.php
+++ schedule.php
@@ -8,10 +8,7 @@ $gregoriandate = jDateTime::toGregorian(2018, 05, 25); // TODO //
 mysqli_query(Init::Db(), "INSERT INTO `Calendar` (`DateKey`, `Jalali`, `Gregorian`) SELECT '" . $datekey . "', '" . $datekey . "', '2018-05-25' WHERE NOT EXISTS (SELECT `DateKey` FROM `Calendar` WHERE DateKey='" . $datekey . "' LIMIT 1);");
 if (isset($_POST['session']))
 {
-  $query = "INSERT INTO `Session` (`Date`, `From`, `To`, `Note`) VALUES ('" . $_POST['date'] . "', '" . $_POST['from'] . "', '" . $_POST['to'] . "', '" . $_POST['note'] . "')";
-  echo $query;
-  exit;
-  mysqli_query(Init::Db(), $query);
+  mysqli_query(Init::Db(), "INSERT INTO `Session` (`Date`, `From`, `To`, `Note`) VALUES ('" . $_POST['date'] . "', '" . $_POST['from'] . "', '" . $_POST['to'] . "', '" . $_POST['note'] . "')");
 }
 ?>
 <!DOCTYPE html>
@@ -27,34 +24,42 @@ if (isset($_POST['session']))
 <body>
 <div class="timetable">
 
-<form method="post" action="schedule.php">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
   <input type="text" name="note" placeholder="توضیحات" />
   <input type="text" placeholder="از ساعت" name="from" data-mask="__:__" />
   <input type="text" placeholder="تا ساعت" name="to" data-mask="__:__" />
   <input type="hidden" name="date" value="<?php echo $datekey ?>" />
   <select name="eventid">
-  <!-- TODO: Group by event type
-  <optgroup label="Outdoors">
-  </optgroup>
-  -->
   <?php
-  $events = mysqli_query(Init::Db(), "SELECT * FROM `Events`;");
-  while($event = $events->fetch_assoc())
+  $eventtypes = mysqli_query(Init::Db(), "SELECT `Type` FROM `Events` GROUP BY `Type`;");
+  while($eventtype = $eventtypes->fetch_assoc())
   {
-    echo '<option value="' . $event["Id"] . '">' . $event["Title"] . '</option>';
+    echo '<optgroup label="' . $eventtype["Type"] . '">';
+    $events = mysqli_query(Init::Db(), "SELECT * FROM `Events` WHERE `Type`='" . $eventtype["Type"] . "';");
+    while($event = $events->fetch_assoc())
+    {
+      echo '<option value="' . $event["Id"] . '">' . $event["Title"] . '</option>';
+    }
+    echo '</optgroup>';
   }
   ?>
   </select>
   <input type="submit" name="session" value="تعریف جلسه" />
-</form>4
-تست
+</form>
+<div class="sessions">
 <?php
-$sessions = mysqli_query(Init::Db(), "SELECT * FROM `Session` WHERE `Date`='" . $datekey . "' ORDER BY `From` ASC;");
+$sessions = mysqli_query(Init::Db(), "SELECT * FROM `Session` WHERE `Date`=" . $datekey . " ORDER BY `From` ASC;");
 while($session = $sessions->fetch_assoc())
 {
-  echo $session["Note"];
+    echo '<div class="session">';
+    echo '<a class="delete" href="schedule.php?date=' . $datekey . '&del=' . $session["Id"] . '">حذف</a>';
+    echo '<a class="edit" href="schedule.php?date=' . $datekey . '&edit=' . $session["Id"] . '">ویرایش</a>';
+    echo '<span class="note">' . $session["Note"] . '</span>';
+    echo '<span class="time">' . substr($session["From"], 0,5) . ' - ' . substr($session["To"],0,5) . '</span>';
+    echo '</div>';
 }
 ?>
+</div>
 </div>
 </body>
 <script type="text/javascript">
