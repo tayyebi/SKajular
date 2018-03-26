aa6f586c4efdc0d5ce93f821a46d377925a7b094
tayyebi <tayyebimohammadreza@gmail.com>
2018-03-27 02:41:18 +0430

    style bugs

--- events.php
+++ events.php
@@ -1,13 +1,7 @@
-<?php //Query 
+<?php
 require_once 'init.php';
 require_once 'datagrid.php';
 
-
- //SELECT 
- 
- $result = mysqli_query(Init::Db()," SELECT Id, Type, Title FROM Events "); 
- if( $result )
- 	table($result);
 
 if (isset($_POST['event']))
 {
@@ -62,12 +56,32 @@ if (isset($_POST['event']))
 <body>
     <form method="post" action="events.php">
     <input type="hidden" name="Id" id="Id" />
-    <label for="Type">Type</label>
-    <input type="text" name="Type" id="Type" />
-    <label for="Title">Title</label>
-    <input type="text" name="Title" id="Title" />
-    <input type="submit" name="event" value="post" />
+    <label for="type">نوع رویداد</label>
+    <select name="type">
+      <option value="COURSE">دوره‌ی آموزشی</option>
+      <option value="OFF">تعطیلی</option>
+      <option value="FESTIVAL">جشنواره</option>
+    </select>
+    <label for="title">عنوان</label>
+    <input type="text" name="title" id="Title" />
+    <button type="submit">
+      <svg version="1.1" class="send-icn" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100px" height="36px" viewBox="0 0 100 36" enable-background="new 0 0 100 36" xml:space="preserve">
+        <path d="M100,0L100,0 M23.8,7.1L100,0L40.9,36l-4.7-7.5L22,34.8l-4-11L0,30.5L16.4,8.7l5.4,15L23,7L23.8,7.1z M16.8,20.4l-1.5-4.3
+	l-5.1,6.7L16.8,20.4z M34.4,25.4l-8.1-13.1L25,29.6L34.4,25.4z M35.2,13.2l8.1,13.1L70,9.9L35.2,13.2z" />
+      </svg>
+      <small>ارسال</small>
+    </button>
+    <a href="index.php">بازگشت</a>
     </form>
+<?php
+
+ 
+$result = mysqli_query(Init::Db()," SELECT Id, Type 'نوع رویداد', Title 'عنوان رویداد' FROM Events "); 
+if( $result )
+  table($result);
+
+?>
 </body>
-<script  src="js/events.js"></script>
+<script src='js/jquery.min.js'></script>
+<script src="js/events.js"></script>
 </html>
